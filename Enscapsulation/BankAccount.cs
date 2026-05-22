2d2e8d5daae2825adbfa9d5b4109b17b34c294e0
RAmKayGift <109849789+RAmKayGift@users.noreply.github.com>
2026-05-22 12:08:18 +0200

    Add files via upload

---
+++ Enscapsulation/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+
+{
+    internal class BankAccount
+    {
+       
+
+        // the wner can be read outside, but only set inside constructor
+        public string Owner { get; }
+
+        // Balance can be read outside but cannot be directly changed outside
+        public decimal Balance { get; private set; }
+
+        
+        private string pin;
+
+        // Constructor
+        public BankAccount(string owner, string accountPin)
+        {
+            Owner = owner;
+            pin = accountPin;
+            Balance = 0;
+        }
+
+        // Checks if the entered pin is correct
+        public bool ValidatePin(string input)
+        {
+            return input == pin;
+        }
+
+        // Deposit money
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Invalid Deposit");
+            }
+
+            Balance = Balance + amount;
+            Console.WriteLine("Deposit Successful");
+            
+        }
+
+        // Withdraw money
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Invalid Withdrawal");
+            }
+
+            if (amount > Balance)
+            {
+                throw new InvalidOperationException("Insufficient Funds.");
+            }
+
+            Balance = Balance - amount;
+            Console.WriteLine("Withrawal Successful");
+        }
+    }
+}
---
+++ Enscapsulation/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int choice = 0;
+            BankAccount account = new BankAccount("Fulufhelo", "2222");
+
+            Console.WriteLine("Authentication");
+            Console.Write("Enter the Pin : ");
+            string pin = Console.ReadLine();
+
+            if(account.ValidatePin(pin)) //execute this if pin is correct
+            {
+                Console.WriteLine("Correct PIN : " + account.ValidatePin("1234"));
+                Console.WriteLine("Owner: " + account.Owner);
+                Console.WriteLine("Balance: R" + account.Balance);
+               
+
+                while(choice != 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Menu \n1.Check Balance \n2.Deposit \n3.Withdraw \n4.Exit");
+                    Console.Write("\tChoice : ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (choice == 1)
+                    {
+                        Console.WriteLine("Balance: R" + account.Balance);
+                    }
+                    else if(choice == 2)
+                    {
+                        Console.Write("Enter doposit Amount : ");
+                        decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+                        account.Deposit(depositAmount);
+                        Console.WriteLine();
+                    }
+                    else if( choice == 3)
+                    {
+                        Console.Write("Enter withdrawal Amount : ");
+                        decimal withdrawalAmount = Convert.ToDecimal(Console.ReadLine());
+                        account.Withdraw(withdrawalAmount);
+                        Console.WriteLine();
+                    }
+                    else if(choice > 4) 
+                    {
+                        Console.WriteLine("Invalid Option");
+                    }
+
+                }
+
+                Console.WriteLine("\tBye");
+                
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong Pin\n  Bye"); //run this if pin is not correct
+            }
+
+            
+
+        }
+    }
+}
