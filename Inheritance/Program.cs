c69b3a44188dba5ea3b3d83d3c7ca929cffb8462
RAmKayGift <109849789+RAmKayGift@users.noreply.github.com>
2026-05-22 12:11:40 +0200

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+
+            vehicles.Add(new Car
+            {
+                Make = "VW",
+                Model = "Polo",
+                Year = 2022,
+                DoorCount = 4
+            });
+
+            vehicles.Add(new Truck
+            {
+                Make = "Isuzu",
+                Model = "F-Series",
+                Year = 2019,
+                PayloadTons = 8.5
+            });
+
+            vehicles.Add(new ElectricCar
+            {
+                Make = "Tesla",
+                Model = "Model x",
+                Year = 2023,
+                DoorCount = 4,
+                BatteryRangeKm = 500
+            });
+
+            foreach (Vehicle vehicle in vehicles)
+            {
+                Console.WriteLine(vehicle.GetDescription());
+            }
+
+        }
+    }
+    
+}
---
+++ Inheritance/Vehicle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    
+        class Vehicle 
+        { 
+            public string Make { get; set; }
+
+            public string Model { get; set; }
+            public int Year { get; set; }
+
+            public virtual string GetDescription()
+            {
+                return Year + " " + Make + " " + Model;
+            }
+           
+            public string GetManufacturer() { return Model; }
+
+         
+        }
+
+        class Car : Vehicle
+        {
+            public int DoorCount { get; set; }
+
+            public override string GetDescription()
+            {
+                return Year + " " + Make + " " + Model + " - Doors: " + DoorCount;
+            }
+
+            
+        }
+
+        class Truck : Vehicle
+        {
+            public double PayloadTons { get; set; }
+
+            public override string GetDescription()
+            {
+                return Year + " " + Make + " " + Model + " - Payload: " + PayloadTons + " tons";
+            }
+        }
+
+        // sealed means no other class can inherit from ElectricCar.
+        sealed class ElectricCar : Car
+        {
+            public int BatteryRangeKm { get; set; }
+
+            public override string GetDescription()
+            {
+                return Year + " " + Make + " " + Model +" , Doors: " + DoorCount +" , Battery Range: " + BatteryRangeKm + " km";
+            }
+        }
+    }
+
